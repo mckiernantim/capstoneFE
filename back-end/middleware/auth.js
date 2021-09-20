7201b5bb5c42f3a5e83ab983c3b69e7a6ecb1b48
Coreen Cooper <coreencooper@pursuit.org>
2021-09-19 22:44:53 -0400

    add validation for friendList

---
+++ back-end/middleware/auth.js
@@ -0,0 +1,20 @@
+const admin = require("../firebase");
+
+// prevent users from accessing users index
+// add to protected route (ex. frontend/UsersIndex.js fetchAllUsers())
+const checkFirebaseToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authtoken;
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        const uid = decodedToken.uid;
+        req.user_id = uid;
+        next();
+    } catch (error) {
+        console.log("Code Broke! ", error);
+        res.status(401).json({message: "No Authenticated User"})
+    }
+}
+
+module.exports = {
+    checkFirebaseToken
+}
