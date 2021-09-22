30fae44a66b409d2f781355eabaa32e8aeb9023d
Coreen Cooper <coreencooper@pursuit.org>
2021-09-22 15:44:15 -0400

    replace div tag with fragment

--- front-end/src/Pages/Connections.js
+++ front-end/src/Pages/Connections.js
@@ -2,9 +2,9 @@ import ConnectionsList from "../Components/ConnectionsList.js";
 
 export const Connections = () => {
   return (
-    <>
+    <section className="connections">
       <h1>Connections Pages</h1>
       <ConnectionsList />
-    </>
+    </section>
   );
 };
